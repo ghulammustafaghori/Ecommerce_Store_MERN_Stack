8d17289727f484cea1b0105798d6a3a324f7f488
Ghulam Mustafa <ghulammustafaghori312484913@gmail.com>
2025-12-09 14:34:58 +0500

    Feature Products Setup Competed

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -8,6 +8,31 @@ const getAllProducts=async(req,res)=>{
    }
 }
 
+const getFeaturedProducts = async (req,res)=>{
+   try{
+      let featuredProducts = await redisClient.get("featured_products");
+      if(featuredProducts){
+         return res.json(JSON.parse(featuredProducts));
+      }
+      //if not in redis, fetch from mongodb
+      // .lean() is gonna return a plain js object instead of mongodb document, which is good for performance
+      featuredProducts= await Product.find({isFeatured:true}).lean();
+
+      if(!featuredProducts){
+         return res.status(404).json({message:"No featured products found"});
+
+      }
+      //store in redis for future quick access
+
+      await redisClient.set("featured_products",JSON.stringify(featuredProducts));
+
+      res.json(featuredProducts);
+   }catch(error){
+      console.log("Error in getFeaturedProducts controller", error.message);
+      res.status(500).json({message:"Server error",error:error.message});
+   }
+}
 
 
-module.exports={getAllProducts};
+
+module.exports={getAllProducts,getFeaturedProducts};
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,17 @@
 const express= require('express');
-const {getAllProducts}=require('../controllers/product.controller');
+const {getAllProducts, getFeaturedProducts}=require('../controllers/product.controller');
 const router = express.Router();
 
 const {protectRoute,adminRoute}=require('../middleware/auth.middleware');
 
+
+
+
 router.get('/',protectRoute,adminRoute,getAllProducts);
+router.get('/featured',getFeaturedProducts);
+router.post('/',productRouter,adminRoute,createProduct)
+
+
+
 
 module.exports=router;
